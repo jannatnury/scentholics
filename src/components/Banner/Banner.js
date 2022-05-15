9f01edaeedab69822158b778c561dfefd0df924c
jannatnury <jannatnury253@gmail.com>
2022-05-16 02:09:11 +0600

    footed edited

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -5,8 +5,11 @@ import img from '../../assets/image/perfume.png'
 
 const Banner = () => {
     return (
-        <div className='banner-container'>
-            <div className='banner-text d-flex flex-column ms-lg-5 col-lg-6 col-sm-12 order-sm-last order-lg-first'>
+        <div className='banner-container row'>
+            <div className='img-container col-lg-6 col-sm-12'>
+                <img src={img} alt="" />
+            </div>
+            <div className='banner-text col-lg-6 col-sm-12'>
                 <p className='mb-0'>We give upto <span className='text-danger fw-bold'>20%</span> discount on products. </p>
                 <h1>Get Your Favourite Perfume <br /> And Smell <span className='text'>Unforgettable</span> </h1>
                 <p className='font-monospace'>Perfume is the key to our memories.Perfume is the art that makes memory speek.You are never fully dressed without perfume..</p>
@@ -14,9 +17,6 @@ const Banner = () => {
                     <button className='button p-2 px-3 fw-bold'>See More</button>
                 </Link>
 
-            </div>
-            <div className='img-container  col-lg-6 col-sm-12 order-sm-first order-lg-last'>
-                <img src={img} alt="" />
             </div>
         </div>
     );
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,34 +1,32 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import './Footer.css';
 import logo from '../../assets/image/logo.png';
+import { toast, ToastContainer } from 'react-toastify';
 
 const Footer = () => {
     return (
         // footer 
         <div className='footer-container'>
-           <div className="col-sm-12 col-lg-4 mt-3 order-sm-last order-lg-first">
-               <img className='logo' src={logo}alt="" />
-               <p>Scentaholics.Inc.provides great deals on our huge collection of perfumes, with unique fragrance, fast shipping, free returns options you'll love</p>
-               <p>Copyright 2022,Jannat Nury</p>
-               <p className='text-danger'>Desclaimer.Terms and Conditions.Privacy Policy.</p>
-           </div>
-           <div className="col-sm-12 col-lg-4 mt-0 order-sm-first order-lg-2">
-               <h5>Quick Links</h5>
-               <Link to="/*">Payment Options</Link>
-               <Link to="/*">FAQ </Link>
-               <Link to="/*">Feedback</Link>
-               <Link to="/*">Return Policy</Link>
-           </div>
-           <div className="col-sm-12 col-lg-4 mt-3 order-sm-2 order-lg-last">
-               <h5>Contract with us</h5>
-               <p>Address : 141,Jannat Nury Street, <br /> Dhaka,Bangladesh</p>
-               <p>Contract us : +0880123456789</p>
-               <h6>Get Best Offers</h6>
-               <input className='bg-white rounded-3 input-field' type="text" />
-               <button className='button' type="submit">Subscribe</button>
 
-           </div>
+            <div className='row d-flex container ms-auto'>
+                <ToastContainer />
+                <div className="col-sm-12 col-lg-6 mt-3 ">
+                    <img className='logo' src={logo} alt="" />
+                    <p>Scentaholics.Inc.provides great deals on our huge collection of perfumes, with unique fragrance, fast shipping, free returns options you'll love</p>
+                    <p>Copyright <small><i class="fas fa-copyright"></i></small> 2022,Jannat Nury</p>
+                    <p className='text-danger'>Desclaimer.Terms and Conditions.Privacy Policy.</p>
+                </div>
+
+                <div className="col-sm-12 col-lg-6 mt-3 align-items-center">
+                    <h5>Contract with us</h5>
+                    <p>Address : 141,Jannat Nury Street, <br /> Dhaka,Bangladesh</p>
+                    <p>Contract us : +0880123456789</p>
+                    <h6>Get Best Offers</h6>
+                    <input className='bg-white rounded-3 input-field' type="email" name="" placeholder='Enter your email' required/>
+                    <button onClick={() => toast.info("Thanks for subscribe")} className='button' type="submit">Subscribe</button>
+
+                </div>
+            </div>
         </div>
     );
 };
