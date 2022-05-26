05accad240b0f3e0a5ee3d3b5bb8d85f6c48bd9d
jannatnury <jannatnury253@gmail.com>
2022-05-26 11:16:58 +0600

    perfumes added

--- src/components/WhyUs/WhyUs.js
+++ src/components/WhyUs/WhyUs.js
@@ -5,13 +5,13 @@ import pic from '../../assets/image/ques.svg';
 const WhyUs = () => {
     return (
         <div className='container shadow py-4 mt-5 m-auto'>
-            <h2 className='title font-monospace mb-0'>Why Choose Us</h2>
+            <h2 className='title font-monospace mb-2'>Why Choose Us</h2>
             <div className='row why-us m-auto container justify-content-evenly align-content-center mb-5 w-75'>
-                <div className='col-sm-12 col-lg-6 order-sm-first'>
-                    <img className='img-fluid ques-img' src={pic} alt="" />
+                <div className='col-sm-12 col-lg-6 order-sm-first mb-3'>
+                    <img className='img-fluid ques-img' src={pic} alt="image" />
                 </div>
                 <div className="col-sm-12 col-lg-6 order-sm-last list-container">
-                    <ul className='list'>
+                    <ul className='list w-100'>
                         <li> 100% Satisfaction Guarantee</li>
                         <li> Free Shipping</li>
                         <li> Free Customiztion</li>
